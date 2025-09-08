d531eeb18f77b9f88492ec09a465cec50da9e86c
sourcerrer <anupthackar@gmail.com>
2025-09-08 18:09:55 +0530

    V0.4.0-dirty1
    Status WIP
    
    + Added mqtt
    
    + Added DNS
    
    + Added TLS and crypto library from NetxDuo

--- Appli/NetXDuo/App/app_netxduo.h
+++ Appli/NetXDuo/App/app_netxduo.h
@@ -32,7 +32,11 @@ extern "C" {
 #include "nx_stm32_eth_driver.h"
 
 /* USER CODE BEGIN Includes */
-
+#include "main.h"
+#include "nxd_dhcp_client.h"
+#include "nxd_mqtt_client.h"
+#include "nxd_sntp_client.h"
+#include "nxd_dns.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
@@ -42,7 +46,45 @@ extern "C" {
 
 /* Exported constants --------------------------------------------------------*/
 /* USER CODE BEGIN EC */
-
+#define MOSQUITTO_CERT_FILE         "mosquitto.cert.h"
+  /* Threads configuration */
+/* Threads configuration */
+#define DEFAULT_MEMORY_SIZE         1024
+#define DEFAULT_MAIN_PRIORITY       10
+#define MQTT_PRIORITY               3
+#define SNTP_PRIORITY               5
+#define LINK_PRIORITY               11
+#define APP_QUEUE_SIZE              10
+/* MQTT Client configuration */
+#define MQTT_CLIENT_STACK_SIZE      1024 * 10
+#define CLIENT_ID_STRING            "MQTT_client_ID"
+#define MQTT_THREAD_PRIORTY         2
+#define MQTT_KEEP_ALIVE_TIMER       60                    /* Define the MQTT keep alive timer for 60 seconds */
+#define CLEAN_SESSION               NX_TRUE
+#define STRLEN(p)                   (sizeof(p) - 1)
+#define TOPIC_NAME                  "Temperature"
+#define NB_MESSAGE                  10                    /*  if NB_MESSAGE = 0, client will publish messages infinitely */
+#define MQTT_BROKER_NAME            "test.mosquitto.org" /* MQTT Server */
+#define MQTT_PORT                   NXD_MQTT_TLS_PORT
+#define QOS0                        0
+#define QOS1                        1
+#define DEMO_MESSAGE_EVENT          1
+#define DEMO_ALL_EVENTS             3
+/* SNTP Client configuration */
+#define SNTP_CLIENT_THREAD_MEMORY    6 * DEFAULT_MEMORY_SIZE
+#define SNTP_SERVER_NAME             "time1.google.com"
+#define SNTP_UPDATE_EVENT            1
+/* Define how often the demo checks for SNTP updates. */
+#define PERIODIC_CHECK_INTERVAL      (60 * NX_IP_PERIODIC_RATE)
+/* Define how often we check on SNTP server status. */
+#define CHECK_SNTP_UPDATES_TIMEOUT   (180 * NX_IP_PERIODIC_RATE)
+#define EPOCH_TIME_DIFF              2208988800     /* is equivalent to 70 years in sec
+                                                     calculated with www.epochconverter.com/date-difference */
+#define NULL_ADDRESS                0
+#define USER_DNS_ADDRESS            IP_ADDRESS(1, 1, 1, 1)   /* User should configure it with his DNS address */
+#define DEFAULT_TIMEOUT             5 * NX_IP_PERIODIC_RATE
+/* This define defines the period of checking the connection of network cable.*/
+#define NX_ETH_CABLE_CONNECTION_CHECK_PERIOD      (1 * NX_IP_PERIODIC_RATE)
 /* USER CODE END EC */
 /* The DEFAULT_PAYLOAD_SIZE should match with RxBuffLen configured via MX_ETH_Init */
 #ifndef DEFAULT_PAYLOAD_SIZE
@@ -70,6 +112,10 @@ extern "C" {
                                                 (addr >> 8) & 0xff,  \
                                                 (addr & 0xff), port, data); \
                                            } while(0)
+
+#define PRINT_CNX_SUCC()          do { \
+                                        printf("SNTP client connected to NTP server : < %s > \n", SNTP_SERVER_NAME);\
+                                     } while(0)
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
